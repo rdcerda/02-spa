3c31752b1e4440e9569556473d5a6ec41714b3f7
RodrigoCerda86 <rodrigo.cerda@bluedott.tech>
2023-10-06 18:10:11 -0300

    feat: subir carpeta src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// rutas
+import { APP_ROUTING } from './app.routes'
 
+// components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroesService } from './services/heroes.service';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { TruncatePipe } from './pipes/truncate.pipe';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
+import { HeroesListComponent } from './components/shared/heroes-list/heroes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent,
+    TruncatePipe,
+    BusquedaComponent,
+    HeroesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
